a1cde1e4fd7573c915be4e03317e37407028ee7f
wxb <wxb0328@gmail.com>
2015-11-08 21:04:18 +0800

    优化,去除google

--- fis-conf.js
+++ fis-conf.js
@@ -1,18 +1,4 @@
 // default settings. fis3 release
-// 
-// fis.match('*', {
-//   deploy: fis.plugin('http-push', {
-//     receiver: 'http://iweb.com/fuelphp/receiver.php',
-//     to: '/usr/local/nginx/www/fuelphp' // 注意这个是指的是测试机器的路径，而非本地机器
-//   })
-// })
-
-fis.media('qa').match('*', {
-  deploy: fis.plugin('http-push', {
-    receiver: 'http://cq.01.p.p.baidu.com:8888/receiver.php',
-    to: '/home/work/htdocs' // 注意这个是指的是测试机器的路径，而非本地机器
-  })
-});
 
 // Global start
 fis.match('*.{js,css}', {
@@ -37,12 +23,39 @@ fis.match('*.png', {
 
 // Global end
 
-// default media is `dev`
+// +++++++++++++++++++++++++++++ extends GLOBAL config +++++++++++++++++++++++++++++++++++++++++++++++
+
+// default media is `dev` fis3 默认media 'dev'，这个'dev'的media是保留字
 fis.media('dev')
   .match('*', {
-    useHash: false,
-    optimizer: null
-  });
+    deploy: fis.plugin('http-push', {
+    receiver: 'http://iweb.com/fuelphp/receiver.php',
+    to: '/usr/local/nginx/www/fuelphp' // 注意这个是指的是测试机器的路径，而非本地机器
+  })
+});
 
-// extends GLOBAL config
-fis.media('production');
+// 开发环境
+fis.media('rd')
+.match('*', {
+ deploy: fis.plugin('http-push', {
+   receiver: 'http://iweb.com/fuelphp/receiver.php',
+   to: '/usr/local/nginx/www/fuelphp' // 注意这个是指的是测试机器的路径，而非本地机器
+ })
+});
+
+// 测试环境
+fis.media('qa')
+.match('*', {
+  deploy: fis.plugin('http-push', {
+    receiver: 'http://cq.01.p.p.baidu.com:8888/receiver.php',
+    to: '/home/work/htdocs' // 注意这个是指的是测试机器的路径，而非本地机器
+  })
+});
+
+// 正式环境
+fis.media('production')
+.match('*', {
+
+});
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
